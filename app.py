3d857ce76fbcd047e2e2cc53b44b671b19c6ae3d
Dawn0415 <135220826+Dawn0415@users.noreply.github.com>
2023-06-02 13:27:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,37 @@
-import urllib.request
-import json
-import jsonpath
-import time
+from flask import Flask,render_template
+import air_update
 
-url=r"https://aqiapp.daqi110.com/report/city/rank/group?group=28&period=0"
-#  请求头
-headers = {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) '
-                            'AppleWebKit/537.36 (KHTML, like Gecko)'
-                            'Chrome/90.0.4430.212 Safari/537.36'}
-#  添加请求头
-req=urllib.request.Request(url,headers=headers)
-#  请求网页，返回响应对象
-response=urllib.request.urlopen(req)
-#  读取json数据并解码
-data=response.read().decode('utf-8')
-#  反序列化成python对象
-json1=json.loads(data)
-#  数据抽取
-#  api空气指数列表
-aqi=jsonpath.jsonpath(json1,'$.list[*].aqi')
-print(aqi)
-#  城市名列表
-city=jsonpath.jsonpath(json1,'$.list[*].city')
-print(city)
-dict={}
-dict["aqi"]=aqi
-dict["city"]=city
 
-# json=json.dumps(dict)
-with open(r"json/实时.json",'w') as f:
-    json.dumps(dict,f)
+app = Flask(__name__)
+@app.route('/index/')
+def index():  # put application's code here
+    return render_template('index.html')
+
+@app.route('/全球变暖/')
+def global_warming():  # put application's code here
+    return render_template('全球变暖.html')
+
+@app.route('/森林面积/')
+def forset_area():  # put application's code here
+    return render_template('森林面积.html')
+
+@app.route('/冰川融化/')
+def melting_glaciers():  # put application's code here
+    return render_template('冰川融化.html')
+
+@app.route('/空气污染/')
+def air_pollution():  # put application's code here
+    return render_template('空气污染.html')
+
+@app.route('/air_data/')
+def air_data():
+    bar = air_update.air_update()
+    return bar.dump_options_with_quotes()
+
+@app.route('/环保倡议/')
+def proposal():  # put application's code here
+    return render_template('环保倡议.html')
+
+
+if __name__ == '__main__':
+    app.run()
